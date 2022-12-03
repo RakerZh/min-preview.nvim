d4b676bc1308ee4f07b45fdec48a5abcfbf0306b
RakerZh <gitrustz10@gmail.com>
2022-12-03 04:57:55 -0500

    update

--- plugin/markdown.lua
+++ plugin/markdown.lua
@@ -1,6 +1,5 @@
-local api = vim.api
 local md = require('markdown')
 
-api.nvim_buf_create_user_command('markdown-preview',function ()
+vim.api.nvim_create_user_command('markdown-preview',function ()
   md:preview()
 end, {})
