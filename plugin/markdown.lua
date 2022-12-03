fa14cdd5f668b123010859f6c12c13a4e95522c1
RakerZh <gitrustz10@gmail.com>
2022-12-03 05:07:58 -0500

    fix invalid name

--- plugin/markdown.lua
+++ plugin/markdown.lua
@@ -1,5 +1,1 @@
-local md = require('markdown')
-
-vim.api.nvim_create_user_command('markdown-preview',function ()
-  md:preview()
-end, {})
+vim.api.nvim_create_user_command('Minpreview', "lua require('markdown').preview()" , {})
